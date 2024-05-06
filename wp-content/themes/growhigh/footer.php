921f6f336fe1045753fc488c236de0de3e7d9771
Laura THAY <laura.thay@ipag.fr>
2024-05-06 19:20:43 +0200

    add import header and footer

---
+++ wp-content/themes/growhigh/footer.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Grow High</title>
+</head>
+<body>
+      <!-- footer -->
+      <footer class="ds-footer">
+      <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-12 col-sm-12  col-md-12  col-lg-10  col-xl-10  col-xxl-10">
+                <section class="ds-lets_section">
+                  <h4>Let’s Talk.</h4>
+                  <span>Have a project or question? Send me a message.</span>
+                </section>
+                <div class="ds-email_social">
+                    <a href="mailto:gnomgnomgnom.studio@gmail.com" class="ds-email ds-boder_text"><span>gnomgnomgnom.studio@gmail.com</span></a>
+                    <ul class="ds-social">
+                        <li><a href="#" target="_blanck"><i class="ri-youtube-line"></i></a></li>
+                        <li><a href="#" target="_blanck"><i class="ri-instagram-line"></i></a></li>
+                        <li><a href="#" target="_blanck"><i class="ri-twitter-line"></i></a></li>
+                    </ul>
+                </div>
+                <span class="ds-copyright">© 2021 All right reserved.<br><a href="https://www.designstub.com/" target="_blanck">Template by Designstub altered by Laura Thay</span>
+            </div>
+          </div>
+      </div>
+    </footer>
+    <!-- footer -->
+    <!-- Optional JavaScript; choose one of the two! -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</body>
+</html>
---
+++ wp-content/themes/growhigh/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Grow High</title>
+</head>
+<body>
+   <!-- header -->
+   <header class="ds-header" id="site-header">
+      <div class="container">
+          <div class="ds-header-inner">
+            <!-- logo -->
+            <a href="index.html" class="ds-logo">
+              <img src="#" alt="logo" class="logo">
+              Grow High
+            </a>
+            <!-- logo -->
+            <!-- social -->
+            <ul class="ds-social">
+              <li><a href="#">E-shop</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Contact</a></li>
+              <li><a href="#" target="_blank"><i class="ri-youtube-fill"></i></a></li>
+              <li><a href="#"><i class="ri-linkedin-fill"></i></a></li>
+            </ul>
+            <!-- social -->
+          </div>
+      </div>
+  </header>
+  <!-- header -->
+</body>
+</html>
