aaf81773139e30e79e052ef5b82570258ea9412a
Laura THAY <laura.thay@ipag.fr>
2024-05-08 22:56:17 +0200

    added style register

--- wp-content/themes/growhigh/functions.php
+++ wp-content/themes/growhigh/functions.php
@@ -5,11 +5,15 @@ function growhigh_supports () {
 }
 
 function growhigh_register_assets () {
-    wp_register_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css');
-    wp_register_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js');
-    wp_register_script('popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js');
-    wp_register_script('jquery', 'https://code.jquery.com/jquery-3.4.1.slim.min.js');
+    wp_register_style('style', get_template_directory_uri() . '/assets/style.css', []);
+    wp_register_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css', []);
+    wp_register_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js', ['popper', 'jquery'], false, true);
+    wp_register_script('popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', [], false, true);
+    wp_deregister_script('jquery');
+    wp_register_script('jquery', 'https://code.jquery.com/jquery-3.4.1.slim.min.js', [], false, true);
+    wp_enqueue_style('style');
     wp_enqueue_style('bootstrap');
+    wp_enqueue_script('bootstrap');
 }
 
 add_action('after_setup_theme', 'growhigh_supports');
