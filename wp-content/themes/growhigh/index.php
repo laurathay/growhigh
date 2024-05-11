a76ef205c99c896347f4327ccfa46872ace71906
Laura THAY <laura.thay@ipag.fr>
2024-05-11 09:34:05 +0200

    init different type

--- wp-content/themes/growhigh/index.php
+++ wp-content/themes/growhigh/index.php
@@ -12,4 +12,45 @@
         <?php else: ?>
             <h1>Pas d'articles</h1>
         <?php endif; ?>
+
+        <div class="ds-slider-section">
+    	<div class="ds-slider">
+        <div class="ds-slider-loop">
+    			<img src="images/tulum.PNG" />
+    			<span><a href="project/yucatan.html">Monstera <i class="ri-external-link-line"></i></a></span>
+    		</div>
+        <div class="ds-slider-loop">
+    			<img src="images/project.png" />
+    			<span><a href="illustration-project.html">Potos <i class="ri-external-link-line"></i></a></span>
+    		</div>
+        <div class="ds-slider-loop">
+    			<img src="images/gurlz.PNG" />
+    			<span><a href="#">Plante du paradis <i class="ri-external-link-line"></i></a></span>
+    		</div>
+    		<div class="ds-slider-loop">
+    			<img src="images/girl.JPG" />
+    			<span><a href="#">Aloe Vera <i class="ri-external-link-line"></i></a></span>
+    		</div>
+    		<div class="ds-slider-loop">
+    			<img src="images/sapa.PNG" />
+    			<span><a href="#">Senseveria <i class="ri-external-link-line"></i></a></span>
+    		</div>
+    		<div class="ds-slider-loop">
+    			<img src="images/hoian.PNG" />
+    			<span><a href="#">Lieres <i class="ri-external-link-line"></i></a></span>
+    		</div>
+    		<div class="ds-slider-loop">
+    			<img src="images/italu.JPG" />
+    			<span><a href="#">Hydroponie <i class="ri-external-link-line"></i></a></span>
+    		</div>
+    		<div class="ds-slider-loop">
+    			<img src="images/lonely.PNG" />
+    			<span><a href="#">Palludarium <i class="ri-external-link-line"></i></a></span>
+    		</div>
+        <div class="ds-slider-loop">
+    			<img src="images/foshobro.jpg" />
+    			<span><a href="#">Nourriture <i class="ri-external-link-line"></i></a></span>
+    		</div>
+
+    </div>
     <?php get_footer() ?>
